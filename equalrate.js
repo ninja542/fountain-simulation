cbd1a0a820afc8871086f4388c32262ef9115dec
ninja542 <theroboticcyborg@gmail.com>
2018-01-12 11:38:44 -0500

    pipe changing finished

--- equalrate.js
+++ equalrate.js
@@ -6,13 +6,19 @@ var pipe = new Vue({
 	},
 	computed: {
 		borderStyle: function(){
-			if(this.pipe1 > this.pipe2){
+			if(this.pipe1 >= this.pipe2){
 				return {borderTop: this.pipe1+"px solid lightskyblue"};
 			}
 			else {
 				return {borderTop: this.pipe2+"px solid lightskyblue",
 								borderLeft: "100px solid transparent"};
 			}
+		},
+		initialVel: function(){
+			return (this.pipe1);
+		},
+		finalVel: function(){
+			return (this.pipe1/this.pipe2)*this.initialVel;
 		}
 	}
 });

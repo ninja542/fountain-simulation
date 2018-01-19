ebb17c018df4dfc8153a588d3db24d695bedbc01
ninja542 <theroboticcyborg@gmail.com>
2018-01-19 12:10:09 -0500

    density update

--- gravity.js
+++ gravity.js
@@ -1,9 +1,12 @@
-// P1 + rgh + 1/2*r*v^2 = P2 + rgh + 1/2*r*v^2
+// P1 + rgh = P2 + rgh
+// velocity is the same so only pressure changes
 var pipe = new Vue({
 	el: "#wrapper",
 	data: {
-		height: 200,
+		height: 2,
 		initialVel: 30,
+		initialPressure: 6000,
+		liquid: 1000,
 	},
 	computed: {
 		finalVel: function(){
@@ -11,13 +14,16 @@ var pipe = new Vue({
 		},
 		positionTop: function(){
 			if (this.height < 0){
-				return {top: -this.height};
+				return {top: -this.height*100};
 			}
 		},
 		positionBottom: function(){
 			if(this.height < 0){
-				return {top: this.height-100};
+				return {top: this.height*100-100};
 			}
-		}
+		},
+		finalPressure: function(){
+			return this.initialPressure - (this.liquid*9.8*-this.height);
+		},
 	}
 });

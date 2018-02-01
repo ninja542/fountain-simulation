bbc6285dd17f008f0335faa6b5e35a6b78143a35
ninja542 <theroboticcyborg@gmail.com>
2018-01-31 21:05:40 -0500

    more stuff

--- equalrate.js
+++ equalrate.js
@@ -1,8 +1,21 @@
-new Vue({
+var margin = { top: 5, right: 5, bottom: 20, left: 40 },
+    width = 1350 - margin.left - margin.right,
+    height = 225 - margin.top - margin.bottom;
+
+var svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+var xScale = d3.scaleLinear().domain([0, 1290]).range([0, 1290]);
+var xAxis = d3.axisBottom(xScale);
+
+var app = new Vue({
 	el: "#wrapper",
 	data: {
 		pipe1: 100,
-		pipe2: 30,
+		pipe2: 75,
 		density: 1000,
 		positionX: 650,
 		initialPressure: 100,
@@ -24,31 +37,95 @@ new Vue({
 		varPressure: function(){
 			return this.initialPressure +
 				(0.5 * this.density * Math.pow(this.initialVel, 2)/1000) -
-				(0.5 * this.density * Math.pow((Math.pow(this.pipe1, 2)/Math.pow(this.connectionHeight, 2))*this.initialVel, 2)/1000);
+				(0.5 * this.density * Math.pow((Math.pow(this.pipe1, 2)/Math.pow(this.connectionHeight(), 2))*this.initialVel, 2)/1000);
 		},
-		connectionHeight: function(){
+	},
+	watch: {
+		finalVel: function(){
+			this.updateLine();
+		}
+	},
+	methods: {
+		varVelocity: function(x = this.positionX){
+			return (Math.pow(this.pipe1, 2)/Math.pow(this.connectionHeight(x), 2))*this.initialVel;
+		},
+		connectionHeight: function(x = this.positionX){
 			if (this.pipe1 <= this.pipe2){
-				if (this.positionX<=(((this.pipe1*100)/this.pipe2)+392)){
+				if (x<=(((this.pipe1*100)/this.pipe2)+392)){
 					return this.pipe1;
 				}
-				else if (this.positionX<=492){
-					return (this.positionX-392)*(this.pipe2/100);
+				else if (x<=492){
+					return (x-392)*(this.pipe2/100);
 				}
 				else {
 					return this.pipe2;
 				}
 			}
 			else {
-				if (this.positionX<=(492-5)){
+				if (x<=(492-5)){
 					return this.pipe1;
 				}
-				else if (this.positionX<=(587-((this.pipe2*100)/this.pipe1))){
-					return this.pipe1-(this.positionX-487)*(this.pipe1/100);
+				else if (x<=(587-((this.pipe2*100)/this.pipe1))){
+					return this.pipe1-(x-487)*(this.pipe1/100);
 				}
 				else {
 					return this.pipe2;
 				}
 			}
+		},
+		animateWater: function(){
+			anime.timeline({
+				loop: true,
+			})
+				.add({
+					targets: ".water-particle",
+					easing: "linear",
+					translateX: 0,
+					duration: 0,
+				})
+				.add({
+					targets: ".water-particle",
+					easing: "linear",
+					translateX: 492+15,
+					duration: ((492+15)/this.initialVel)*1000,
+				})
+				.add({
+					targets: ".water-particle",
+					easing: "linear",
+					translateX: 1275,
+					duration: ((1275-492)/this.finalVel)*1000,
+				});
+		},
+		updateLine: function(){
+			d3.select(".yaxis").call(d3.axisLeft(this.yScale()));
+			var line = d3.line()
+				.x(function(d){return xScale(d);})
+				.y(function(d){return app.yScale(app.varVelocity(d));});
+			svg.append("path").attr("d", line(Array.from(Array(1290).keys())))
+				.attr("stroke", "black").attr("stroke-width", 1).attr("fill", "none").attr("class", "line");
+			// svg.select(".line").attr("d", line(Array.from(Array(1290).keys())))
+			// 	.attr("stroke", "black").attr("stroke-width", "1px").attr("fill", "none");
+		},
+		yScale: function(){
+			if (this.finalVel < this.initialVel) {
+				return d3.scaleLinear().domain([0, this.initialVel]).range([200, 0]);
+			}
+			else {
+				return d3.scaleLinear().domain([0, this.finalVel]).range([200, 0]);
+			}
 		}
+	},
+	mounted: function(){
+		var scaleY = this.yScale();
+		var yAxis = d3.axisLeft(scaleY);
+		svg.append("g").call(xAxis).attr("transform", "translate(" + 0 + ", " + scaleY(0) + ")");
+		svg.append("g").call(yAxis).attr("class", "yaxis");
+
+		var velocity = this.varVelocity();
+		var line = d3.line()
+			.x(function(d){return xScale(d);})
+			.y(function(d){return scaleY(velocity(d));});
+		svg.append("path").attr("d", line(Array.from(Array(1290).keys())))
+			.attr("stroke", "black").attr("stroke-width", 1).attr("fill", "none").attr("class", "line");
 	}
 });
--- gravity.js
+++ gravity.js
@@ -7,6 +7,7 @@ new Vue({
 		initialVel: 30,
 		initialPressure: 100,
 		liquid: 1000,
+		positionX: 650,
 	},
 	computed: {
 		finalVel: function(){
@@ -25,5 +26,18 @@ new Vue({
 		finalPressure: function(){
 			return this.initialPressure - (this.liquid*9.8*-this.height)/1000;
 		},
+		varPressure: function(){
+			return this.initialPressure +
+				(0.5 * this.density * Math.pow(this.initialVel, 2)/1000) -
+				(0.5 * this.density * Math.pow((Math.pow(this.pipe1, 2)/Math.pow(this.connectionHeight, 2))*this.initialVel, 2)/1000);
+		},
+		connectionHeight: function(){
+			if (this.positionX > 492 && this.positionX < 592){
+				return 100;
+			}
+			else {
+				return 100;
+			}
+		}
 	}
 });
